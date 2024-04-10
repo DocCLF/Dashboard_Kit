14f0ce14a59d86769fad015e77fd54b8e8a3c1d7
ToolDoc <mailtodoc@web.de>
2024-04-10 17:23:06 +0200

    reworked portbuffershow

--- Scripts/FOS-Mainfile.ps1
+++ Scripts/FOS-Mainfile.ps1
@@ -168,25 +168,36 @@ foreach ($FOS_port in $FOS_perrsh_temp){
 #endregion
 
 #region Portbuffershow
+
+$FOS_InfoCount = $FOS_advInfo.count
+0..$FOS_InfoCount |ForEach-Object {
+    # Pull only the effective ZoneCFG back into ZoneList
+    if($FOS_advInfo[$_] -match 'Buffers$'){
+        $FOS_pbs_temp = $FOS_advInfo |Select-Object -Skip $_
+        $FOS_Temp_var = $FOS_pbs_temp |Select-Object -Skip 2
+        #break
+    }
+}
+
 # $test = ssh admin@192.168.249.81 "portbuffershow" 
-$FOS_Temp_var = $FOS_pbs_temp |Select-Object -Skip 3
+#$FOS_Temp_var = $FOS_pbs_temp |Select-Object -Skip 3
 #$FOS_pbs_temp = plink insight@192.168.107.40 -pw Insight0Mon -batch "portbuffershow" 
-$FOS_pbs_temp = Get-Content -Path ".\pbs_l.txt"
-$FOS_Temp_var = $FOS_pbs_temp |Select-Object -Skip 3
+#$FOS_pbs_temp = Get-Content -Path ".\pbs_l.txt"
+#$FOS_Temp_var = $FOS_pbs_temp |Select-Object -Skip 3
 $FOS_pbs =@()
 foreach ($FOS_thisLine in $FOS_Temp_var) {
     #create a var and pipe some objects in and fill them with some data
     $FOS_PortBuff = "" | Select-Object Port,Type,Mode,Max_Resv,Tx,Rx,Usage,Buffers,Distance,Buffer
-    $FOS_PortBuff.Port = $FOS_thisLine.Substring(0,4).Trim()
-    $FOS_PortBuff.Type = $FOS_thisLine.Substring(11,4).Trim()
-    $FOS_PortBuff.LX_Mode = $FOS_thisLine.Substring(17,4).Trim()
-    $FOS_PortBuff.Max_Resv = $FOS_thisLine.Substring(27,7).Trim()
-    $FOS_PortBuff.Tx = $FOS_thisLine.Substring(36,14).Trim()
-    $FOS_PortBuff.Rx = $FOS_thisLine.Substring(50,14).Trim()
-    $FOS_PortBuff.Usage = $FOS_thisLine.Substring(67,6).Trim()
-    $FOS_PortBuff.Buffers = $FOS_thisLine.Substring(75,7).Trim()
-    $FOS_PortBuff.Distance = $FOS_thisLine.Substring(85,6).Trim(" ","-")
-    $FOS_PortBuff.Buffer = $FOS_thisLine.Substring(95,6).Trim()
+    $FOS_PortBuff.Port = ($FOS_thisLine |Select-String -Pattern '^\s+(\d+)' -AllMatches).Matches.Groups.Value[1]
+    $FOS_PortBuff.Type = ($FOS_thisLine |Select-String -Pattern '([EFGLU])' -AllMatches).Matches.Groups.Value[1]
+    $FOS_PortBuff.LX_Mode = ($FOS_thisLine |Select-String -Pattern '(LE|LD|L0|LS)' -AllMatches).Matches.Groups.Value[1]
+    $FOS_PortBuff.Max_Resv = ($FOS_thisLine |Select-String -Pattern '(\d+)\s+(\d+\(|-\s\()' -AllMatches).Matches.Groups.Value[1]
+    $FOS_PortBuff.Tx = ($FOS_thisLine |Select-String -Pattern '(\d+\(\d+\)|\d\(\s\d+\)|-\s\(\s\d+\)|-\s\(\s+\d+\)|-\s\(\d+\)|-\s\(\s+-\s+\))' -AllMatches).Matches.Groups.Value[1]
+    $FOS_PortBuff.Rx = ($FOS_thisLine |Select-String -Pattern '(\d+\(\d+\)|\d\(\s\d+\)|-\s\(\s\d+\)|-\s\(\s+\d+\)|-\s\(\d+\)|-\s\(\s+-\s+\))' -AllMatches).Matches.Value[1]
+    $FOS_PortBuff.Usage = ($FOS_thisLine |Select-String -Pattern '\)\s+(\d+)\s+' -AllMatches).Matches.Groups.Value[1]
+    $FOS_PortBuff.Buffers = ($FOS_thisLine |Select-String -Pattern '\)\s+(\d+)\s+(\d+|-)' -AllMatches).Matches.Groups.Value[2]
+    $FOS_PortBuff.Distance = ($FOS_thisLine |Select-String -Pattern '\d\s+(\d+|-)\s+(\d+km|\<\d+km|-)' -AllMatches).Matches.Groups.Value[2]
+    $FOS_PortBuff.Buffer = ($FOS_thisLine |Select-String -Pattern '\s+(\d+)$' -AllMatches).Matches.Groups.Value[1]
     $FOS_pbs += $FOS_PortBuff
 
 }

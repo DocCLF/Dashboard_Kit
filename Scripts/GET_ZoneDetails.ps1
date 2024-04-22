2c680ca7bc3adc8f3a90dbe4c6a54dc57399c4de
ToolDoc <mailtodoc@web.de>
2024-04-22 15:50:21 +0200

    add some comments and code cleaned up at GET_ZoneDetails

--- Scripts/GET_ZoneDetails.ps1
+++ Scripts/GET_ZoneDetails.ps1
@@ -1,39 +1,27 @@
 using namespace System.Net
 
-function GET_ZoneDetails {
-        <#
-        .SYNOPSIS
-        Displays zone information.
+function GET_ZoneDetails  {
+    <#
+    .SYNOPSIS
+    Displays zone information.
 
-        .DESCRIPTION
-        Use this command to display zone configuration information. 
-        This command includes sorting and search options to customize the output. 
-        If a pattern is specified, the command displays only matching zone configuration names in the defined configuration. 
-        When used without operands, the command displays all zone configuration information for the Defined and the Effective configuration.        
+    .DESCRIPTION
+    Use this command to display zone configuration information. 
+    This command includes sorting and search options to customize the output. 
+    If a pattern is specified, the command displays only matching zone configuration names in the defined configuration. 
+    When used without operands, the command displays all zone configuration information for the Defined and the Effective configuration.        
 
-        .EXAMPLE
-        Displays configuration information for all Peer Zones
-        FOS_Zone_Details -UserName admin -SwitchIP 10.10.10.25 -FOS_Operand peerzone
-
-        Display all zones
-        FOS_Zone_Details -UserName admin -SwitchIP 10.10.10.25
+    .EXAMPLE
+    Display all zones
+    GET_ZoneDetails
         
-        .LINK
-        Brocade® Fabric OS® Command Reference Manual, 9.2.x
-        https://techdocs.broadcom.com/us/en/fibre-channel-networking/fabric-os/fabric-os-commands/9-2-x/Fabric-OS-Commands.html
-        #>
-   <# param (
-        [Parameter(Mandatory,ValueFromPipeline)]
-        [string]$UserName,
-        [Parameter(Mandatory,ValueFromPipeline)]
-        [ipaddress]$SwitchIP,
-        [Parameter(ValueFromPipeline)]
-        [ValidateSet("peerzone")]
-        [string]$FOS_Operand
-    )#>
+    .LINK
+    Brocade® Fabric OS® Command Reference Manual, 9.2.x
+    https://techdocs.broadcom.com/us/en/fibre-channel-networking/fabric-os/fabric-os-commands/9-2-x/Fabric-OS-Commands.html
+    #>
+
     begin{
-        Write-Debug -Message "Begin block |$(Get-Date)"
-        # create some var
+        Write-Debug -Message "Begin GET_ZoneDetails |$(Get-Date)"
 
         $FOS_ZoneCollection = @()
 
@@ -41,33 +29,25 @@ function GET_ZoneDetails {
         
     }
     process{
-        Write-Debug -Message "Start of Process block |$(Get-Date)"
+        Write-Debug -Message "Start of Process from GET_ZoneDetails |$(Get-Date)"
         # Creat a list of Aliase with WWPN based on the decision by AliasName, with a "wildcard" there is only a list similar Aliasen or without a Aliasname there will be all Aliases of the cfg in the List.
-        switch ($FOS_Operand) {
-            "peerzone" { 
-                $FOS_ZoneList = ssh $UserName@$($SwitchIP) "zoneshow --peerzone all "
-                
-                Write-Debug -Message "FOS_Operand $FOS_Operand`n, SearchZoneName: $FOS_ZoneName`n, FilterMode $FOS_Mode`n, Zoneliste`n $FOS_ZoneList, `nZoneEntrys`n $FOS_ZoneEntrys, `nZoneCount`n $FOS_ZoneCount "
-             }
-            Default {
 
-                $FOS_BasicZoneList = Get-Content -Path ".\Schl_Fab2.txt"
-                #$FOS_BasicZoneList = ssh $UserName@$($SwitchIP) "zoneshow"
-                $FOS_ZoneCount = $FOS_BasicZoneList.count
+        $FOS_BasicZoneList = Get-Content -Path ".\Schl_Fab2.txt"
+        #$FOS_BasicZoneList = ssh $UserName@$($SwitchIP) "zoneshow"
+        $FOS_ZoneCount = $FOS_BasicZoneList.count
 
-                0..$FOS_ZoneCount |ForEach-Object {
-                    # Pull only the effective ZoneCFG back into ZoneList
-                    if($FOS_BasicZoneList[$_] -match '^Effective'){
-                        $FOS_ZoneList = $FOS_BasicZoneList |Select-Object -Skip $_
-                        break
-                    }
-                }
-
-                Write-Debug -Message "FOS_Operand Default`n, Search: zoneshow`n, Zoneliste`n $FOS_ZoneCount, `nZoneEntrys`n $FOS_BasicZoneList, `nZoneCount`n $FOS_ZoneList "
+        0..$FOS_ZoneCount |ForEach-Object {
+            # Pull only the effective ZoneCFG back into ZoneList
+            if($FOS_BasicZoneList[$_] -match '^Effective'){
+                $FOS_ZoneList = $FOS_BasicZoneList |Select-Object -Skip $_
+                break
             }
         }
+
+        Write-Debug -Message "FOS_Operand Default`n, Search: zoneshow`n, Zoneliste`n $FOS_ZoneCount, `nZoneEntrys`n $FOS_BasicZoneList, `nZoneCount`n $FOS_ZoneList "
+
         # is not necessary, but even a system needs a break from time to time
-        Start-Sleep -Seconds 3;
+        Start-Sleep -Seconds 2;
 
         # Creat a List of Aliases with WWPN based on switch-case decision
         if(($FOS_ZoneList.count) -ge 4){
@@ -82,16 +62,18 @@ function GET_ZoneDetails {
                 }elseif(Select-String -InputObject $FOS_Zone -Pattern '(:[\da-f]{2}:[\da-f]{2}:[\da-f]{2})$') {
                     $FOS_AliWWN = $FOS_Zone
                     $FOS_TempCollection.WWPN = $FOS_AliWWN.Trim()
+                    <# Boolean to control the do until loop #>
                     $FOS_DoUntilLoop = $true
                     foreach($FOS_BasicZoneListTemp in $FOS_BasicZoneList){
+                        <# Start of the do until loop #>
                         do {
                             if($FOS_BasicZoneListTemp -match '^ alias:\s(.*)'){
                                 Write-Debug -Message "$FOS_BasicZoneListTemp "
                                 $FOS_TeampAliasName = $FOS_BasicZoneListTemp
                                 $FOS_TempAliasName = $FOS_TeampAliasName -replace '^ alias:\s',''.Trim()
                                 break
-                                $FOS_SwInfo
                             }
+
                             if($FOS_BasicZoneListTemp -match ($FOS_AliWWN.Trim())){
                                 Write-Debug -Message " $FOS_BasicZoneListTemp "
                                 $FOS_DoUntilLoop = $false
@@ -101,13 +83,14 @@ function GET_ZoneDetails {
                             break
                             
                         } until (
-                            
+
                             $FOS_DoUntilLoop -eq $true
                         )
+                        <# Boolean to control the do until loop with break out option #>
                         If($FOS_DoUntilLoop -eq $false){break}
                     }
 
-                    #Write-Host "$FOS_AliName`n, $FOS_Zone" -ForegroundColor DarkYellow
+                    Write-Debug -Message "$FOS_AliName`n, $FOS_Zone"
                 }else{
                     <# Action when all if and elseif conditions are false #>
                     Write-Host "`n"
@@ -129,7 +112,6 @@ function GET_ZoneDetails {
     }
     end{
         # clear the most of the used vars
-        #Clear-Variable FOS_* -Scope Global;
         Write-Debug -Message "End block |$(Get-Date)"
     }
 }

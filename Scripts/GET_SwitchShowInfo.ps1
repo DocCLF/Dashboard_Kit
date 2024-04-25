4b29fa322de32291fa0b07f0a9dbb6695a830c93
ToolDoc <mailtodoc@web.de>
2024-04-25 19:11:38 +0200

    run ScriptAnalyzer with sone fix infos

--- Scripts/GET_SwitchShowInfo.ps1
+++ Scripts/GET_SwitchShowInfo.ps1
@@ -7,14 +7,13 @@ function GET_SwitchShowInfo {
     .DESCRIPTION
         Use this command to display switch, blade, and port status information. Output may vary depending on the switch model.
     .NOTES
-        
+        Need infos to be added   
     .LINK
         Brocade® Fabric OS® Command Reference Manual, 9.2.x
         https://techdocs.broadcom.com/us/en/fibre-channel-networking/fabric-os/fabric-os-commands/9-2-x/Fabric-OS-Commands/switchShow_921.html
     .EXAMPLE
         GET_BasicSwitchInfos -FOS_MainInformation $yourvarobject
     #>
-    
     [CmdletBinding()]
     param (
         [Parameter(Mandatory)]
@@ -23,7 +22,7 @@ function GET_SwitchShowInfo {
     
     begin {
 
-        Write-Debug -Message "Start Func GET_SwitchShowInfo |$(Get-Date)` "
+        Write-Debug -Message "Start Func GET_SwitchShowInfo |$(Get-Date)`n "
 
         <#----- Array for information of the switchports ----#>
         $FOS_SwBasicPortDetails=@()
@@ -34,7 +33,7 @@ function GET_SwitchShowInfo {
     
     process {
 
-        Write-Debug -Message "Process Func GET_SwitchShowInfo |$(Get-Date)` "
+        Write-Debug -Message "Process Func GET_SwitchShowInfo |$(Get-Date)`n "
 
         foreach($FOS_linebyLine in $FOS_MainInformation){
 
@@ -72,10 +71,10 @@ function GET_SwitchShowInfo {
     end {
 
         <# returns the hashtable for further processing, not mandatory but the safe way #>
-        Write-Debug -Message "End Func GET_SwitchShowInfo |$(Get-Date)` "
+        Write-Debug -Message "End Func GET_SwitchShowInfo |$(Get-Date)`n "
 
-        Write-Debug -Message "$(Get-Date) return: ` $FOS_SwBasicPortDetails ` "
-        Write-Debug -Message "$(Get-Date) return: ` $FOS_usedPorts ` "
+        Write-Debug -Message "$(Get-Date) return:`n $FOS_SwBasicPortDetails `n "
+        Write-Debug -Message "$(Get-Date) return:`n $FOS_usedPorts `n "
         
         return $FOS_SwBasicPortDetails, $FOS_usedPorts
         

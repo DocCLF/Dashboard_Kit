49bf9f539c36b825ee4986e27b6a87a558b8b762
ToolDoc <mailtodoc@web.de>
2024-05-02 21:39:29 +0200

    rework select string and start-end Data selection

--- Scripts/GET_SwitchShowInfo.ps1
+++ Scripts/GET_SwitchShowInfo.ps1
@@ -28,6 +28,17 @@ function GET_SwitchShowInfo {
         $FOS_SwBasicPortDetails=@()
         <#----- Array for information of the used switchports ----#>
         $FOS_usedPorts =@()
+
+        $FOS_InfoCount = $FOS_MainInformation.count
+        Write-Debug -Message "Number of Lines: $FOS_InfoCount "
+        0..$FOS_InfoCount |ForEach-Object {
+            # Pull only the effective ZoneCFG back into ZoneList
+            if($FOS_MainInformation[$_] -match '^Index'){
+                if($FOS_MainInformation[$_] -match '^\s+frames'){break}
+                $FOS_SWShowTemp = $FOS_MainInformation |Select-Object -Skip $_
+                $FOS_SwShowArry_temp = $FOS_SWShowTemp |Select-Object -Skip 2   
+            }
+        }
         
     }
     
@@ -35,28 +46,28 @@ function GET_SwitchShowInfo {
 
         Write-Debug -Message "Process Func GET_SwitchShowInfo |$(Get-Date)`n "
 
-        foreach($FOS_linebyLine in $FOS_MainInformation){
+        foreach($FOS_linebyLine in $FOS_SwShowArry_temp){
 
             <# Only collect data up to the next section, marked by frames #>
             if($FOS_linebyLine -match '^\s+frames'){break}
     
             # Build the Portsection of switchshow
-            if($FOS_linebyLine -match '^\s+\d+'){
+            if($FOS_linebyLine -match '^\s+\d+'){   # (\d+\.\d\w|\d+)
                 $FOS_SWsh = "" | Select-Object Index,Port,Address,Media,Speed,State,Proto,PortConnect
                 <# Port index is a number between 0 and the maximum number of supported ports on the platform. The port index identifies the port number relative to the switch. #>
-                $FOS_SWsh.Index = $FOS_linebyLine.Substring(0,4).Trim()
+                $FOS_SWsh.Index = ($FOS_linebyLine |Select-String -Pattern '^\s+(\d+)' -AllMatches).Matches.Groups.Value[1]
                 <# Port number; 0-15, 0-31, or 0-63. #>
-                $FOS_SWsh.Port = $FOS_linebyLine.Substring(5,5).Trim()
+                $FOS_SWsh.Port = ($FOS_linebyLine |Select-String -Pattern '^\s+\d+\s+(\d+)' -AllMatches).Matches.Groups.Value[1]
                 <# The 24-bit Address Identifier. #>
-                $FOS_SWsh.Address = $FOS_linebyLine.Substring(10,8).Trim()
+                $FOS_SWsh.Address = ($FOS_linebyLine |Select-String -Pattern '(\d+[a-z]+\d+|\d+)' -AllMatches).Matches.Value[3]
                 <# Media types means module types #>
-                $FOS_SWsh.Media = $FOS_linebyLine.Substring(20,4).Trim()
+                $FOS_SWsh.Media = ($FOS_linebyLine |Select-String -Pattern '\s+(id|--|cu)\s+' -AllMatches).Matches.Groups.Value[1]
                 <# The speed of the port. #>
-                $FOS_SWsh.Speed = $FOS_linebyLine.Substring(25,5).Trim()
+                $FOS_SWsh.Speed = ($FOS_linebyLine |Select-String -Pattern '\s+(id|--|cu)\s+(N\d+|\d+G|AN|UN)' -AllMatches).Matches.Groups.Value[2]
                 <# Port state information #>
-                $FOS_SWsh.State = $FOS_linebyLine.Substring(34,10).Trim()
+                $FOS_SWsh.State = ($FOS_linebyLine |Select-String -Pattern '(\w+_\w+|\w+)\s+(FC)' -AllMatches).Matches.Groups.Value[1]
                 <# Protocol support by GbE port. #>
-                $FOS_SWsh.Proto = $FOS_linebyLine.Substring(45,4).Trim()
+                $FOS_SWsh.Proto = ($FOS_linebyLine |Select-String -Pattern '(\w+_\w+|\w+)\s+(FC)' -AllMatches).Matches.Groups.Value[2]
                 <# WWPN or other Infos #>
                 $FOS_SWsh.PortConnect = $FOS_linebyLine.Substring(50).Trim()
                 $FOS_SwBasicPortDetails += $FOS_SWsh

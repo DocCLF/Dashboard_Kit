d76766216e769e18fd5f5754da5b6ff8a657ab02
ToolDoc <mailtodoc@web.de>
2024-05-08 15:38:48 +0200

    modif collection of Infos for the Hashtable, if FOS is less then 8.2.x

--- Scripts/GET_BasicSwitchInfos.ps1
+++ Scripts/GET_BasicSwitchInfos.ps1
@@ -29,50 +29,20 @@ function GET_BasicSwitchInfos {
         <# Hashtable for BasicSwitch Info #>
         $FOS_SwGeneralInfos =[ordered]@{}
 
-        <# Collect all needed Infos #>
-        $FOS_FW_Info = ($FOS_MainInformation | Select-String -Pattern '([v?][\d]\.[\d+]\.[\d]\w)$' -AllMatches).Matches.Value |Select-Object -Unique
-        $FOS_IP_AddrCFG = ($FOS_MainInformation | Select-String -Pattern '(?:[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})').Matches.Value |Select-Object -Unique
-        $FOS_DHCP_CFG = (($FOS_MainInformation | Select-String -Pattern '^DHCP:\s(\w+)$' -AllMatches).Matches.Value |Select-Object -Unique).Trim('DHCP: ')
-        $FOS_temp = ($FOS_MainInformation | Select-String -Pattern 'switchType:\s(.*)$','switchState:\s(.*)$','switchRole:\s(.*)$' |ForEach-Object {$_.Matches.Groups[1].Value}).Trim()
-
-        Write-Debug -Message "FOS-Version $FOS_FW_Info ` IP-CFG $FOS_IP_AddrCFG ` DHCP $FOS_DHCP_CFG ` Switch $FOS_temp ` $(Get-Date)` "
-
-        if($FOS_temp[2] -like "1*"){
-            # with VF jump in here
-            switch ($FOS_temp[2]) {
-                {$_ -like "109*"}  { $FOS_SwHw = "Brocade 6510" }
-                {$_ -like "118*"}  { $FOS_SwHw = "Brocade 6505" }
-                {$_ -like "170*"}  { $FOS_SwHw = "Brocade G610" }
-                {$_ -like "162*"}  { $FOS_SwHw = "Brocade G620" }
-                {$_ -like "183*"}  { $FOS_SwHw = "Brocade G620" }
-                {$_ -like "173*"}  { $FOS_SwHw = "Brocade G630" }
-                {$_ -like "184*"}  { $FOS_SwHw = "Brocade G630" }
-                {$_ -like "178*"}  { $FOS_SwHw = "Brocade 7810 Extension Switch" }
-                {$_ -like "181*"}  { $FOS_SwHw = "Brocade G720" }
-                {$_ -like "189*"}  { $FOS_SwHw = "Brocade G730" }
-                Default {$FOS_SwHw = "Unknown Type"}
-            }
-            $FOS_StateTemp =$FOS_temp[3]
-            $FOS_RoleTemp =$FOS_temp[4]
-        }else {
-            # else jump in here
-            switch ($FOS_temp[1]) {
-                {$_ -like "109*"}  { $FOS_SwHw = "Brocade 6510" }
-                {$_ -like "118*"}  { $FOS_SwHw = "Brocade 6505" }
-                {$_ -like "170*"}  { $FOS_SwHw = "Brocade G610" }
-                {$_ -like "162*"}  { $FOS_SwHw = "Brocade G620" }
-                {$_ -like "183*"}  { $FOS_SwHw = "Brocade G620" }
-                {$_ -like "173*"}  { $FOS_SwHw = "Brocade G630" }
-                {$_ -like "184*"}  { $FOS_SwHw = "Brocade G630" }
-                {$_ -like "178*"}  { $FOS_SwHw = "Brocade 7810 Extension Switch" }
-                {$_ -like "181*"}  { $FOS_SwHw = "Brocade G720" }
-                {$_ -like "189*"}  { $FOS_SwHw = "Brocade G730" }
-                Default {$FOS_SwHw = "Unknown Type"}
-            }
-            $FOS_StateTemp =$FOS_temp[2]
-            $FOS_RoleTemp =$FOS_temp[3]
+        switch (($FOS_MainInformation | Select-String -Pattern 'switchType:\s+(\d.*)$' |ForEach-Object {$_.Matches.Groups[1].Value})) {
+            {$_ -like "109*"}  { $FOS_SwHw = "Brocade 6510" }
+            {$_ -like "118*"}  { $FOS_SwHw = "Brocade 6505" }
+            {$_ -like "170*"}  { $FOS_SwHw = "Brocade G610" }
+            {$_ -like "162*"}  { $FOS_SwHw = "Brocade G620" }
+            {$_ -like "183*"}  { $FOS_SwHw = "Brocade G620" }
+            {$_ -like "173*"}  { $FOS_SwHw = "Brocade G630" }
+            {$_ -like "184*"}  { $FOS_SwHw = "Brocade G630" }
+            {$_ -like "178*"}  { $FOS_SwHw = "Brocade 7810 Extension Switch" }
+            {$_ -like "181*"}  { $FOS_SwHw = "Brocade G720" }
+            {$_ -like "189*"}  { $FOS_SwHw = "Brocade G730" }
+            Default {$FOS_SwHw = "Unknown Type"}
         }
-        
+
     }
     
     process {
@@ -80,13 +50,16 @@ function GET_BasicSwitchInfos {
 
         # add more Basic Infos of the switch to the Hashtable
         $FOS_SwGeneralInfos.Add('Brocade Product Name',$FOS_SwHw)
-        $FOS_SwGeneralInfos.Add('FOS Version',$FOS_FW_Info)
-        $FOS_SwGeneralInfos.Add('Ethernet IP Address',$FOS_IP_AddrCFG[0])
-        $FOS_SwGeneralInfos.Add('Ethernet Subnet mask',$FOS_IP_AddrCFG[1])
-        $FOS_SwGeneralInfos.Add('Gateway IP Address',$FOS_IP_AddrCFG[2])
-        $FOS_SwGeneralInfos.Add('DHCP',$FOS_DHCP_CFG)
-        $FOS_SwGeneralInfos.Add('Switch State',$FOS_StateTemp)
-        $FOS_SwGeneralInfos.Add('Switch Role',$FOS_RoleTemp)
+
+        foreach ($lineUp in $FOS_MainInformation) {
+            if($lineUp -match '^Index'){break}
+            $FOS_SwGeneralInfos.Add('Ethernet IP Address',(($lineUp| Select-String -Pattern 'Ethernet IP Address:\s+([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})').Matches.Groups[1].Value))
+            $FOS_SwGeneralInfos.Add('Ethernet Subnet mask',(($lineUp| Select-String -Pattern 'Ethernet Subnet mask:\s+([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})').Matches.Groups[1].Value))
+            $FOS_SwGeneralInfos.Add('Gateway IP Address',(($lineUp| Select-String -Pattern 'Gateway IP Address:\s+([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3})').Matches.Groups[1].Value))
+            $FOS_SwGeneralInfos.Add('DHCP',((($lineUp| Select-String -Pattern '^DHCP:\s(\w+)$' -AllMatches).Matches.Groups[1].Value)))
+            $FOS_SwGeneralInfos.Add('Switch State',(($lineUp| Select-String -Pattern 'switchState:\s+(.*)$').Matches.Groups[1].Value))
+            $FOS_SwGeneralInfos.Add('Switch Role',(($lineUp| Select-String -Pattern 'switchRole:\s+(.*)$').Matches.Groups[1].Value))
+        }
         
     }
     
